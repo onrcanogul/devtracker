8e385c2d1ea9f4335b1f7b1636a4479196bde527
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-06-07 17:02:54 +0300

    retry ignore

--- common/src/main/java/com/devtracker/common/configuration/RabbitConfigurer.java
+++ common/src/main/java/com/devtracker/common/configuration/RabbitConfigurer.java
@@ -1,5 +1,7 @@
 package com.devtracker.common.configuration;
 
+import com.devtracker.common.exception.BadRequestException;
+import com.devtracker.common.exception.NotFoundException;
 import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
@@ -8,6 +10,11 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RabbitConfigurer {
@@ -21,16 +28,27 @@ public class RabbitConfigurer {
             ConnectionFactory connectionFactory,
             AcknowledgeMode acknowledgeMode,
             int maxAttempts) {
+
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setAcknowledgeMode(acknowledgeMode);
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
+
+
+        Map<Class<? extends Throwable>, Boolean> retryableExceptions = new HashMap<>();
+        retryableExceptions.put(NotFoundException.class, false);
+        retryableExceptions.put(BadRequestException.class, false);
+
+        RetryPolicy retryPolicy = new SimpleRetryPolicy(maxAttempts, retryableExceptions, true);
+
         factory.setAdviceChain(RetryInterceptorBuilder.stateless()
-                .maxAttempts(maxAttempts)
-                .recoverer(new RejectAndDontRequeueRecoverer())
+                .retryPolicy(retryPolicy)
+                .recoverer(new RejectAndDontRequeueRecoverer()) // DLQ
                 .build());
+
         return factory;
     }
+
 
 
 }
