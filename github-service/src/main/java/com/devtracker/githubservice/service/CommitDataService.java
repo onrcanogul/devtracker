7ba8f53b6b9e69df29cfdc1e4fbada4ccab4d4ed
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-06-07 20:52:54 +0300

    exception was implemented

--- github-service/src/main/java/com/devtracker/githubservice/service/CommitDataService.java
+++ github-service/src/main/java/com/devtracker/githubservice/service/CommitDataService.java
@@ -4,7 +4,8 @@ import com.devtracker.common.service.BaseService;
 import com.devtracker.common.util.ServiceResponse;
 import com.devtracker.githubservice.dto.CommitDataDto;
 import com.devtracker.githubservice.entity.CommitData;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface CommitDataService extends BaseService<CommitData, CommitDataDto> {
-    ServiceResponse<CommitDataDto> create(CommitDataDto model);
+    ServiceResponse<CommitDataDto> create(CommitDataDto model) throws JsonProcessingException;
 }
