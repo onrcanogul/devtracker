9aa1031e5ef11d2cf45c28c1d8d9d921dfe06229
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-06-07 20:44:28 +0300

    implement

--- github-service/src/main/java/com/devtracker/githubservice/service/CommitDataService.java
+++ github-service/src/main/java/com/devtracker/githubservice/service/CommitDataService.java
@@ -1,8 +1,10 @@
 package com.devtracker.githubservice.service;
 
 import com.devtracker.common.service.BaseService;
+import com.devtracker.common.util.ServiceResponse;
 import com.devtracker.githubservice.dto.CommitDataDto;
 import com.devtracker.githubservice.entity.CommitData;
 
 public interface CommitDataService extends BaseService<CommitData, CommitDataDto> {
+    ServiceResponse<CommitDataDto> create(CommitDataDto model);
 }
